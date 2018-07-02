b4d478c434623b17b004b61803136ef2c4af0eb1
Wojciech Zając <wojciechzajaaac@gmail.com>
2018-07-02 14:37:55 +0200

    Update the code comments

--- js/app.js
+++ js/app.js
@@ -1,7 +1,6 @@
 // Enemies our player must avoid
 class Enemy {
-    // Variables applied to each of our instances go here,
-    // we've provided one for you to get started
+    // Variables applied to each of our instances go here
 
     // The image/sprite for our enemies, this uses
     // a helper we've provided to easily load images
@@ -15,7 +14,7 @@ class Enemy {
 // Update the enemy's position, required method for game
 // Parameter: dt, a time delta between ticks
     update(dt) {
-    // You should multiply any movement by the dt parameter
+    // Multiply any movement by the dt parameter
     // which will ensure the game runs at the same speed for
     // all computers.
     this.x += this.speed * dt;
@@ -33,7 +32,7 @@ class Enemy {
 };
 
 
-// Now write your own player class
+// Our Player
 // This class requires an update(), render() and
 // a handleInput() method.
 class Player {
@@ -48,77 +47,99 @@ class Player {
     }
 
     update() {
+        // If the player is dead = hit an enemy for the 5th time
         if(this.dead) {
+            // Open a Game Over modal
             openModal();
         }
     }
 
+    // Draw the player on the screen, required method for game
     render() {
         ctx.drawImage(Resources.get(this.sprite), this.x, this.y);
     }
 
     handleInput(keyPressed) {
+        // Declaclare the player's range of movements
         const xJump = 100;
         const yJump = 82;
         const maxLeftPostiion = 5;
         const water = -10;
         const maxRightPostition = 405;
         const maxDownPosition = 400;
-        //If the player tries to go off screen, he's set to the max frame position
+        
         switch(keyPressed) {
+            // If you press the Left arrow on keyboard, the player moves by the pre-defined jump value
             case 'left':
                 this.x -= xJump;
+                    // If the player tries to go off screen, he's set to the max frame position
                     if (this.x <= maxLeftPostiion) {
                         this.x = maxLeftPostiion;
                     }
             break;
+            // If you press the Up arrow on keyboard, the player moves by the pre-defined jump value
             case 'up':
                 this.y -= yJump;
+                    // If the player tries to go off screen, he's set to the max frame position
                     if (this.y <= water) {
                         this.y = water;
                     }
             break;
+            // If you press the Reft arrow on keyboard, the player moves by the pre-defined jump value
             case 'right':
                 this.x += xJump;
+                    // If the player tries to go off screen, he's set to the max frame position
                     if (this.x >= maxRightPostition) {
                         this.x = maxRightPostition;
                     }
             break;
+            // If you press the Deft arrow on keyboard, the player moves by the pre-defined jump value
             case 'down':
                 this.y += yJump;
+                    // If the player tries to go off screen, he's set to the max frame position
                     if (this.y >= maxDownPosition) {
                         this.y = maxDownPosition;
                     }
             break;
         }
-        //Player reaches the water
+        // Player reaches the water
         if (this.y === water) {
+            // Player scores
             this.win();
             setTimeout( () => {
-              this.restart();
-              }, 350);    
+                // Player goes to the start
+                this.restart();
+            }, 350);    
         }
     }
 }
 
 
-Player.prototype.win = function() { 
+Player.prototype.win = function() {
+    // Add 1 point to the score 
     this.score++;
+    // Update the score on screen
     document.querySelector('.score').textContent = this.score;
 }
 
 
 Player.prototype.die = function() {
-    player.lives--;
+    // Substract 1 player's life (of 5)
+    this.lives--;
+    // Update the number of lives on screen
     document.querySelector('.lives').textContent = this.lives;
     
+    // If the player reaches 0 lives
     if (this.lives === 0) {
+        // Set the death property
         this.dead = true;
+        // And attach the earned points to modal
         attachResult();
     }
 }
 
 
+// Re-set the player to the starting box (grass)
 Player.prototype.restart = function() {
     this.x = 205;
     this.y = 400;
@@ -126,29 +147,33 @@ Player.prototype.restart = function() {
 
 
 Player.prototype.resetScore = function() {
+    // Re-set properties to the original ones
     this.score = 0;
     this.lives = 5;
     this.dead = false;
+    // Update the score and number of lives on screen (back to the original ones)
     document.querySelector('.score').textContent = this.score;
     document.querySelector('.lives').textContent = this.lives; 
 }
 
 
-// Now instantiate your objects.
-// Place all enemy objects in an array called allEnemies
+// Instantiate the enemies objects.
 const enemy1 = new Enemy(-180, 65, 140);
 const enemy2 = new Enemy(-500, 145, 180);
 const enemy3 = new Enemy(-400, 230, 100);
+
+
+// Place all enemy objects in an array
 const allEnemies = [];
 allEnemies.push(enemy1, enemy2, enemy3);
 
 
-// Place the player object in a variable called player
+// Place the player object
 const player = new Player(205, 400);
 
 
 // This listens for key presses and sends the keys to your
-// Player.handleInput() method. You don't need to modify this.
+// Player.handleInput() method.
 document.addEventListener('keyup', function(e) {
     var allowedKeys = {
         37: 'left',
@@ -161,6 +186,7 @@ document.addEventListener('keyup', function(e) {
 });
 
 
+// This opens the Game Over modal
 function openModal() {
     const modal = document.querySelector('.modal');
 
@@ -168,6 +194,7 @@ function openModal() {
 }
 
 
+//T his closes the Game Over modal
 function closeModal() {
     const modal = document.querySelector('.modal');
 
@@ -176,9 +203,10 @@ function closeModal() {
 }
 
 
+// This displays the score on modal
 function attachResult() {
     let result = document.querySelector('.result');
-    
+    // Plural/Singular output form validator
     if (player.score > 1 || player.score === 0) {
         result.textContent = `${player.score} times.`;
     } else {
@@ -187,6 +215,9 @@ function attachResult() {
 }
 
 
+// This listens for clicks on the Play Again button and restarts the game
+// This should be called after the DOM is fully loaded
+// Otherwise - it will return null
 window.onload = () => {
     const button = document.querySelector('.button');
 
